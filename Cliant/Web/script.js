98ae20d5abbce920514b961ce7fd3e39586b1b08
oosan1 <akaharaimori555@gmail.com>
2025-01-30 20:53:59 +0900

    シリアルの読み取り処理を追加(バグあり)

--- Cliant/Web/script.js
+++ Cliant/Web/script.js
@@ -9,13 +9,10 @@ connectButton.addEventListener("click", onConnectButtonClick, false);
 serialConsoleTextbox.addEventListener('keydown', onConsoleKeypress);
 sendButton.addEventListener("click", sendMessage, false);
 
-navigator.serial.addEventListener("connect", (event) => {
-    const button = document.getElementById("send_btn");
-    button.disabled = false;
-});
 navigator.serial.addEventListener("disconnect", (event) => {
     const button = document.getElementById("send_btn");
     button.disabled = true;
+    sendSerialConsole("disconnection", "red");
 });
 
 function onConsoleKeypress(event) {
@@ -26,7 +23,7 @@ function onConsoleKeypress(event) {
 
 // シリアル通信関係
 async function onConnectButtonClick() {
-    let baudrate;
+    let baudrate
     try {
         baudrate = Number(baudrateTextbox.value);
     } catch (e) {
@@ -38,8 +35,14 @@ async function onConnectButtonClick() {
         console.log("接続成功")
         const button = document.getElementById("send_btn");
         button.disabled = false;
+        sendSerialConsole("connection", "green");
+        readTextSerial();
     } catch (e) {
         console.error(`Connection failed ${e}`)
+        sendSerialConsole("Connection failed", "red");
+        sendSerialConsole("==============================", "red");
+        sendSerialConsole(e, "red");
+        sendSerialConsole("==============================", "red");
     }
 }
 
@@ -60,12 +63,34 @@ function sendMessage() {
     serialConsoleTextbox.value = "";
 }
 
-function sendSerialConsole(text) {
+function sendSerialConsole(text, color) {
     const serialConsole = document.getElementById("serial_console");
 
     const messageElement = document.createElement("div");
     messageElement.classList.add("message");
-    messageElement.textContent = messageText;
+    messageElement.classList.add(color);
+    messageElement.textContent = text;
 
     serialConsole.prepend(messageElement);
+}
+
+async function readTextSerial() {
+    while (port.readable) {
+        const reader = port.readable.getReader();
+        try {
+            while (true) {
+                const { value, done } = await reader.read();
+                if (done) {
+                  console.log("Canceled");
+                  break;
+                }
+                const inputValue = new TextDecoder().decode(value);
+                sendSerialConsole(inputValue, "black");
+              }
+        } catch (error) {
+            console.log(error);
+        }finally {
+            reader.releaseLock();
+        }
+    }
 }
