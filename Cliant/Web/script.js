6e1365ad2e650c7f49abe466051f986fdf34a616
oosan1 <akaharaimori555@gmail.com>
2025-01-30 18:07:00 +0900

    シリアルコンソールの表示部分を追加

--- Cliant/Web/script.js
+++ Cliant/Web/script.js
@@ -2,8 +2,18 @@ let port;
 
 const connectButton = document.getElementById("connect_btn");
 const baudrateTextbox = document.getElementById("baudrate");
+const serialConsoleTextbox = document.getElementById("send_text");
+const sendButton = document.getElementById("send_btn");
 
 connectButton.addEventListener("click", onConnectButtonClick, false);
+serialConsoleTextbox.addEventListener('keydown', onConsoleKeypress);
+sendButton.addEventListener("click", sendMessage, false);
+
+function onConsoleKeypress(event) {
+    if(event.key === 'Enter'){
+		sendMessage();
+	}
+}
 
 async function onConnectButtonClick() {
     let baudrate;
@@ -19,4 +29,19 @@ async function onConnectButtonClick() {
     } catch (e) {
         console.error(`Connection failed ${e}`)
     }
+}
+
+//デバッグ用
+function sendMessage() {
+    const serialConsole = document.getElementById("serial_console");
+    const messageText = serialConsoleTextbox.value.trim();
+
+    if (messageText === "") return;
+
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("message");
+    messageElement.textContent = messageText;
+
+    serialConsole.prepend(messageElement);
+    serialConsoleTextbox.value = "";
 }
