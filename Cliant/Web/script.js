a6e92805b060f19f094a1707ea4b7e95e5efbcd8
oosan1 <akaharaimori555@gmail.com>
2025-01-30 18:33:12 +0900

    シリアルの書き込み機能を追加

--- Cliant/Web/script.js
+++ Cliant/Web/script.js
@@ -9,12 +9,22 @@ connectButton.addEventListener("click", onConnectButtonClick, false);
 serialConsoleTextbox.addEventListener('keydown', onConsoleKeypress);
 sendButton.addEventListener("click", sendMessage, false);
 
+navigator.serial.addEventListener("connect", (event) => {
+    const button = document.getElementById("send_btn");
+    button.disabled = false;
+});
+navigator.serial.addEventListener("disconnect", (event) => {
+    const button = document.getElementById("send_btn");
+    button.disabled = true;
+});
+
 function onConsoleKeypress(event) {
     if(event.key === 'Enter'){
 		sendMessage();
 	}
 }
 
+// シリアル通信関係
 async function onConnectButtonClick() {
     let baudrate;
     try {
@@ -26,22 +36,36 @@ async function onConnectButtonClick() {
         port = await navigator.serial.requestPort();
         await port.open({ baudRate: baudrate });
         console.log("接続成功")
+        const button = document.getElementById("send_btn");
+        button.disabled = false;
     } catch (e) {
         console.error(`Connection failed ${e}`)
     }
 }
 
-//デバッグ用
+async function writeTextSerial(text) {
+    const encoder = new TextEncoder();
+    const writer = port.writable.getWriter();
+    await writer.write(encoder.encode(text));
+    console.log("テキスト書き込み: " + text);
+    writer.releaseLock();
+}
+
 function sendMessage() {
-    const serialConsole = document.getElementById("serial_console");
     const messageText = serialConsoleTextbox.value.trim();
 
     if (messageText === "") return;
+
+    writeTextSerial(messageText);
+    serialConsoleTextbox.value = "";
+}
+
+function sendSerialConsole(text) {
+    const serialConsole = document.getElementById("serial_console");
 
     const messageElement = document.createElement("div");
     messageElement.classList.add("message");
     messageElement.textContent = messageText;
 
     serialConsole.prepend(messageElement);
-    serialConsoleTextbox.value = "";
 }
