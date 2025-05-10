37a880d28e18c17c61f5b2d45aa4834fe7d58467
oosan1 <akaharaimori555@gmail.com>
2025-05-10 09:52:01 +0900

    マイコンのEISを反転回路に対応

--- microcomputer/C_C++/SMUcontrol/SMUcontrol.c
+++ microcomputer/C_C++/SMUcontrol/SMUcontrol.c
@@ -20,6 +20,7 @@
 
 // DAC設定
 #define PIN_LDAC 9
+#define SPI_CLOCK_SPEED 15 * MHZ
 
 // ADC設定
 #define ADC_STEP 4096
@@ -27,7 +28,7 @@
 #define IV_BUF_SIZE 10000
 
 // システムクロック設定 (67~260MHz)
-#define SYSTEM_CLOCK_MHZ 200
+#define SYSTEM_CLOCK_MHZ 250
 
 // プロトタイプ宣言
 int INFO(datetime_t *t);
@@ -133,8 +134,8 @@ int IVsweep(int channel, float speed_VperS, int voltage_step_max) {
     return 0;
 }
 
-// IVcurve {DACchannel(0:A, 1:B)} {ADCchannel} {speed(V/s)} {waitingTime(us)} {maxVoltageStep(step表記)} {&result_list} {&result_size} {&cal_list} {&isCalibrated}
-int IVcurve(int DACchannel, int ADCchannel, float speed_VperS, int waiting_time, int voltage_step_max, uint16_t *result_list, int *result_size, int *cal_list, bool *isCalibrated) {
+// IVcurve {DACchannel(0:A, 1:B)} {ADCchannel} {speed(V/s)} {waitingTime(us)} {maxVoltageStep(step表記)} {offset_voltage_step}(step表記) {isInvert} {&result_list} {&result_size} {&cal_list} {&isCalibrated}
+int IVcurve(int DACchannel, int ADCchannel, float speed_VperS, int waiting_time, int voltage_step_max, int offset_voltage_step, bool isInvert, uint16_t *result_list, int *result_size, int *cal_list, bool *isCalibrated) {
     char buffer[512];
     if(ADCchannel < 0 || ADCchannel > 4) {
         sendLog("Available ADC channels are 1 to 3.", 3);
@@ -172,6 +173,7 @@ int IVcurve(int DACchannel, int ADCchannel, float speed_VperS, int waiting_time,
     const float conversionFactor = 3.3f / (1 << 12);
     adc_select_input(ADCchannel);
     uint16_t ADCvalue;
+    int ADCvoltage_step;
     float ADCvoltage;
     *result_size = voltage_step_max + 1;
 
@@ -214,7 +216,12 @@ int IVcurve(int DACchannel, int ADCchannel, float speed_VperS, int waiting_time,
     }
     printf("START\n");
     for (int i = 0; i <= voltage_step_max; i++) {
-        ADCvoltage = (result_list[i] - cal_list[result_list[i]]) * conversionFactor;
+        ADCvoltage_step =  result_list[i] - cal_list[result_list[i]];
+        ADCvoltage = ADCvoltage_step * conversionFactor;
+        if (isInvert) {
+            ADCvoltage = (ADCvoltage - ADC_REF) * -1;
+        }
+        ADCvoltage += ADC_REF * ((float)offset_voltage_step / ADC_STEP);
         printf("%f %f\n", 3.3 / 4096 * i, ADCvoltage);
     }
     printf("END\n");
@@ -225,8 +232,8 @@ int IVcurve(int DACchannel, int ADCchannel, float speed_VperS, int waiting_time,
     return 0;
 }
 
-// IVcal {resistance(Ω)} {&IV_list} {&IV_size} {&cal_list} {&isCalibrated}
-int IVcal(float resistance, uint16_t *IV_list, int *IV_size, int *cal_list, bool *isCalibrated) {
+// IVcal {resistance(Ω)} {offset_voltage_step}(step表記) {isInvert} {&IV_list} {&IV_size} {&cal_list} {&isCalibrated}
+int IVcal(float resistance, float VtoIresistance, int offset_voltage_step, bool isInvert, uint16_t *IV_list, int *IV_size, int *cal_list, bool *isCalibrated) {
     char buffer[512];
     if(resistance <= 0) {
         sendLog("The calibration resistance value must be greater than 0.\n", 3);
@@ -244,15 +251,22 @@ int IVcal(float resistance, uint16_t *IV_list, int *IV_size, int *cal_list, bool
     const float conversionFactor = 3.3f / (1 << 12);
     int theoretical_vol;
     int vol_diff;
+    int vol_step;
 
     sendLog("Start calibration data calculation.\n", 1);
     for (int i = 0; i < *IV_size; i++) {
-        theoretical_vol = ((3.3 / ADC_STEP * i) / resistance * 100) / 3.3 * ADC_STEP;
-        vol_diff = IV_list[i] - theoretical_vol;
-        if(cal_list[IV_list[i]] == -ADC_STEP) {
-            cal_list[IV_list[i]] = vol_diff;
+        vol_step = IV_list[i];
+
+        theoretical_vol = ((3.3 / ADC_STEP * i) / resistance * VtoIresistance) / 3.3 * ADC_STEP;
+        theoretical_vol -= offset_voltage_step;
+        if (isInvert) {
+            theoretical_vol = (theoretical_vol - ADC_STEP) * -1;
+        }
+        vol_diff = vol_step - theoretical_vol;
+        if(cal_list[vol_step] == -ADC_STEP) {
+            cal_list[vol_step] = vol_diff;
         }else {
-            cal_list[IV_list[i]] = (int)((cal_list[IV_list[i]] + vol_diff) / 2);
+            cal_list[vol_step] = (int)((cal_list[vol_step] + vol_diff) / 2);
         }
     }
     sendLog("End calibration data calculation.\n", 1);
@@ -272,8 +286,8 @@ int IVcal(float resistance, uint16_t *IV_list, int *IV_size, int *cal_list, bool
     return 0;
 }
 
-// EIS {DACchannel(0:A, 1:B)} {ADCchannel} {samplingRate(Hz)} {raise_time(ms)} {Voltage_min(step)} {Voltage_max(step)} {repeat_count} {&result_list} {&result_size} {&cal_list} {&isCalibrated}
-int EIS(int DACchannel, int ADCchannel, float samplingRate, float raise_time, int volage_min, int volage_max, int repeat_count, uint16_t *result_list, int *result_size, int *cal_list, bool *isCalibrated) {
+// EIS {DACchannel(0:A, 1:B)} {ADCchannel} {samplingRate(Hz)} {raise_time(ms)} {Voltage_min(step)} {Voltage_max(step)} {repeat_count} {offset_voltage_step}(step)} {isInvert} {&result_list} {&result_size} {&cal_list} {&isCalibrated}
+int EIS(int DACchannel, int ADCchannel, float samplingRate, float raise_time, int volage_min, int volage_max, int repeat_count, int offset_voltage_step, bool isInvert, uint16_t *result_list, int *result_size, int *cal_list, bool *isCalibrated) {
     printf("DAC: %d, ADC: %d, samplingRate: %f, raise_time: %f, volage_min: %d, volage_max: %d, ", DACchannel, ADCchannel, samplingRate, raise_time, volage_min, volage_max);
     char buffer[512];
     if(ADCchannel < 0 || ADCchannel > 4) {
@@ -313,8 +327,9 @@ int EIS(int DACchannel, int ADCchannel, float samplingRate, float raise_time, in
     const float conversionFactor = 3.3f / (1 << 12);
     adc_select_input(ADCchannel);
     uint16_t ADCvalue;
+    int ADCvoltage_step;
     float ADCvoltage;
-    *result_size = loop_count;
+    *result_size = loop_count*repeat_count;
     
     // IVcurve測定
     sendLog("Start measurement.\n", 1);
@@ -326,19 +341,21 @@ int EIS(int DACchannel, int ADCchannel, float samplingRate, float raise_time, in
     sleep_ms(100);
 
     for (int i = 0; i <= loop_count*repeat_count; i++) {
-        if (loop_count_half % i == 0) {
+        if (i % loop_count_half == 0 && i != 0) {
             if (voltage_status) {
                 gpio_put(PIN_CS, 0);
                 spi_write16_blocking(SPI_PORT, &data_min_voltage, 2);
                 gpio_put(PIN_CS, 1);
                 gpio_put(PIN_LDAC, 0);
                 gpio_put(PIN_LDAC, 1);
+                voltage_status = false;
             } else {
                 gpio_put(PIN_CS, 0);
                 spi_write16_blocking(SPI_PORT, &data_max_voltage, 2);
                 gpio_put(PIN_CS, 1);
                 gpio_put(PIN_LDAC, 0);
                 gpio_put(PIN_LDAC, 1);
+                voltage_status = true;
             }
         }
         target_time_us = start_time_us + wait_time_us;
@@ -354,16 +371,12 @@ int EIS(int DACchannel, int ADCchannel, float samplingRate, float raise_time, in
 
     // 計測後は安全のため、出力電圧を0Vに戻す。
 
-    uint16_t write_data;
-    for (int i = volage_max; i >= 0; i--) {
-        write_data = DAC_setting_data + i;
-        gpio_put(PIN_CS, 0);
-        spi_write16_blocking(SPI_PORT, &write_data, 2);
-        gpio_put(PIN_CS, 1);
-        gpio_put(PIN_LDAC, 0);
-        gpio_put(PIN_LDAC, 1);
-        sleep_ms(10);
-    }
+    uint16_t write_data = DAC_setting_data + 0;
+    gpio_put(PIN_CS, 0);
+    spi_write16_blocking(SPI_PORT, &write_data, 2);
+    gpio_put(PIN_CS, 1);
+    gpio_put(PIN_LDAC, 0);
+    gpio_put(PIN_LDAC, 1);
     
     // 測定データの送信
     sendLog("Start sending.\n", 1);
@@ -373,8 +386,13 @@ int EIS(int DACchannel, int ADCchannel, float samplingRate, float raise_time, in
         printf("CALIBRATION:OFF\n");
     }
     printf("START\n");
-    for (int i = 0; i <= loop_count; i++) {
-        ADCvoltage = (result_list[i] - cal_list[result_list[i]]) * conversionFactor;
+    for (int i = 0; i <= loop_count*repeat_count; i++) {
+        ADCvoltage_step =  result_list[i] - cal_list[result_list[i]];
+        ADCvoltage = ADCvoltage_step * conversionFactor;
+        if (isInvert) {
+            ADCvoltage = (ADCvoltage - ADC_REF) * -1;
+        }
+        ADCvoltage += ADC_REF * ((float)offset_voltage_step / ADC_STEP);
         printf("%f %f\n", (1 / samplingRate) * i, ADCvoltage);
     }
     printf("END\n");
@@ -421,7 +439,7 @@ int main() {
     stdio_init_all();
 
     // SPI初期化
-    spi_init(SPI_PORT, 10 * MHZ);
+    spi_init(SPI_PORT, SPI_CLOCK_SPEED);
     spi_set_format( SPI_PORT,
                     16,
                     0,
@@ -471,6 +489,8 @@ int main() {
     int IVcal_list[IV_BUF_SIZE] = {0};
     bool isCalibrated = false;
     int IVcurve_size = 0;
+    int offset_voltage_step = 0;
+    bool isInvert = false;
 
     sendLog("system started\n", 1);
 
@@ -525,7 +545,7 @@ int main() {
             scanf("%f", &float_com_arg1);
             scanf("%d", &int_com_arg3);
             scanf("%d", &int_com_arg4);
-            success = IVcurve(int_com_arg1, int_com_arg2, float_com_arg1, int_com_arg3, int_com_arg4, IVcurve_list, &IVcurve_size, IVcal_list, &isCalibrated);
+            success = IVcurve(int_com_arg1, int_com_arg2, float_com_arg1, int_com_arg3, int_com_arg4, offset_voltage_step, isInvert, IVcurve_list, &IVcurve_size, IVcal_list, &isCalibrated);
             if(success==0) {
                 sendLog("IVcurve was executed\n", 0);
             }else {
@@ -533,9 +553,10 @@ int main() {
             }
         }
         else if(strcmp(com_command, "IVcal") == 0) {
-            // IVcal {resistance(Ω)} {&IV_list} {&IV_size} {&cal_list}
+            // IVcal {resistance(Ω)} {VtoIresistance(Ω)}{&IV_list} {&IV_size} {&cal_list}
             scanf("%f", &float_com_arg1);
-            success = IVcal(float_com_arg1, IVcurve_list, &IVcurve_size, IVcal_list, &isCalibrated);
+            scanf("%f", &float_com_arg2);
+            success = IVcal(float_com_arg1, float_com_arg2, offset_voltage_step, isInvert, IVcurve_list, &IVcurve_size, IVcal_list, &isCalibrated);
             if(success==0) {
                 sendLog("IVcal was executed\n", 0);
             }else {
@@ -552,12 +573,23 @@ int main() {
             scanf("%d", &int_com_arg6);
             scanf("%d", &int_com_arg7);
 
-            success = EIS(int_com_arg1, int_com_arg2, float_com_arg1, float_com_arg2, int_com_arg5, int_com_arg6, int_com_arg7, IVcurve_list, &IVcurve_size, IVcal_list, &isCalibrated);
+            success = EIS(int_com_arg1, int_com_arg2, float_com_arg1, float_com_arg2, int_com_arg5, int_com_arg6, int_com_arg7, offset_voltage_step, isInvert, IVcurve_list, &IVcurve_size, IVcal_list, &isCalibrated);
             if(success==0) {
-                sendLog("IVcal was executed\n", 0);
+                sendLog("EIS was executed\n", 0);
             }else {
-                sendLog("IVcal was failed\n", 3);
+                sendLog("EIS was failed\n", 3);
             }
+        }
+        else if(strcmp(com_command, "setOffsets") == 0) {
+            // setOffsets {offset_voltage(step)} {isInvert(0:false, 1:true)}
+            scanf("%d", &int_com_arg1);
+            scanf("%d", &int_com_arg2);
+
+            offset_voltage_step = int_com_arg1;
+            isInvert = int_com_arg2;
+            sprintf(buffer, "offset_voltage:%d, isInvert:%d\n", offset_voltage_step, isInvert);
+            sendLog(buffer, 0);
+            sendLog("setOffsets was executed\n", 0);
         }
         else {
             sprintf(buffer, "Unknown command:%s\n", com_command);
